b0a6b05f05849e99109a60bcc32cf74ba9ff79bb
IvanCHern88 <92941716+IvanCHern88@users.noreply.github.com>
2021-11-18 18:06:05 +0200

    Update task_3.py

--- HT_1/task_3.py
+++ HT_1/task_3.py
@@ -1,1 +1,10 @@
+# 3. Write a script to sum of the first n positive integers.
 
+print("Введіть числа через кому без відступу:")
+a=input().split(',')
+sum=0
+for i in a:
+    i=int(i)
+    if i>0:
+        sum+=i
+print(sum)
