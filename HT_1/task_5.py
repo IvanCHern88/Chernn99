ebbcb01a45af3e3c7508a4098a67e4b41f6a5a1f
IvanCHern88 <92941716+IvanCHern88@users.noreply.github.com>
2021-11-18 19:33:22 +0200

    Update task_5.py

--- HT_1/task_5.py
+++ HT_1/task_5.py
@@ -1,7 +1,8 @@
-# 5. Write a script to convert decimal to hexadecimal
+"""
+5. Write a script to convert decimal to hexadecimal
         Sample decimal number: 30, 4
         Expected output: 1e, 04
-        
+"""        
         
 a=input('Введіть Послідовність чисел через кому без відступу: ').split(',')
 for i in range(len(a)):
