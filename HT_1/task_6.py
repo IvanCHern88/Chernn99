40423973a3fee74782610b947645f84cc120a1e2
IvanCHern88 <92941716+IvanCHern88@users.noreply.github.com>
2021-11-18 18:32:58 +0200

    Update task_6.py

--- HT_1/task_6.py
+++ HT_1/task_6.py
@@ -1,1 +1,11 @@
-
+# 6. Write a script to check whether a specified value is contained in a group of values.
+        Test Data :
+        3 -> [1, 5, 8, 3] : True
+        -1 -> (1, 5, 8, 3) : False
+          
+print('Введіть числа в список через кому без відступу:')
+a=input().split(',') # створює список строк 
+for i in range(len(a)):
+    a[i]=int(a[i])
+b=int(input('Введіть число яке хочете перевірити у списку: '))    
+print(b,"-->",a,b in a,":",sep=' ')
