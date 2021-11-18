59d9e1abb8bbfb4a1a70ac7d0ddc58b4ba4595ef
IvanCHern88 <92941716+IvanCHern88@users.noreply.github.com>
2021-11-18 17:39:50 +0200

    Update task_1.py

--- HT_1/task_1.py
+++ HT_1/task_1.py
@@ -1,1 +1,11 @@
+#  1 .Write a script which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers.
+        Sample data : 1, 5, 7, 23
+        Output :
+        List : [‘1', ' 5', ' 7', ' 23']
+        Tuple : (‘1', ' 5', ' 7', ' 23')
 
+a=input('Введіть Послідовність чисел через кому без відступу: ').split(',')
+b=list(a)
+c=tuple(a)
+print("Список:",b,sep=' ')
+print("Кортеж:",c,sep=' ')                 
