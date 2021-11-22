b44df3fae78b82c8bd4f2044a84daf97c0bf1890
IvanCHern88 <92941716+IvanCHern88@users.noreply.github.com>
2021-11-22 17:22:57 +0200

    Update task_4.py

--- HT_2/task_4.py
+++ HT_2/task_4.py
@@ -7,3 +7,15 @@
         Expected Result : {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
 """
 
+def task4(dict_a, dict_b, dict_c):
+    res_dict = dict_a
+    res_dict.update(dict_b)
+    res_dict.update(dict_c)
+    return res_dict
+
+
+dict1 = {1: 10, 2: 200, 3: 210}
+dict2 = {4: 30, 5: 40}
+dict3 = {6: 50, 7: 60}
+
+print(task4(dict1, dict2, dict3))
