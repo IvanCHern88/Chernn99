63e97291ec3ddc3a9b89d626ba3eacab1791816c
IvanCHern88 <92941716+IvanCHern88@users.noreply.github.com>
2021-11-22 16:51:41 +0200

    Update task_7.py

--- HT_2/task_7.py
+++ HT_2/task_7.py
@@ -8,3 +8,12 @@
                 
 """
 
+dictionary = {1 : 11, 2 : 2, 3 : 89, 4: 32, 5 : 188}
+
+max_element = max(list(dictionary.values()))
+min_element = min(list(dictionary.values()))
+
+print(f"MAX: {max_element}")
+print(f"MIN: {min_element}")
+
+
