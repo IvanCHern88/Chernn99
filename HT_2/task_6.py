df46ca168bf2b7b8fdd793ea1723faa01d49ef7c
IvanCHern88 <92941716+IvanCHern88@users.noreply.github.com>
2021-11-22 16:54:11 +0200

    Update task_6.py

--- HT_2/task_6.py
+++ HT_2/task_6.py
@@ -7,3 +7,12 @@
         Expected Result : dict_1 = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
 """
 
+first_dictionary = {1: 11, 2: 22, 3: 33}
+second_dictionary = {4: 44, 5: 55, 6: 66}
+third_dictionary = {7: 77, 8: 88, 9: 99}
+
+first_dictionary.update(second_dictionary)
+first_dictionary.update(third_dictionary)
+
+print(f"first_dictionary = {first_dictionary}")
+
