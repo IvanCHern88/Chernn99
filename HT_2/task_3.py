9933fa0eab3ae602a284cdc229b34709d8376740
IvanCHern88 <92941716+IvanCHern88@users.noreply.github.com>
2021-11-22 17:16:43 +0200

    Update task_3.py

--- HT_2/task_3.py
+++ HT_2/task_3.py
@@ -4,3 +4,8 @@
         Expected output: [('',), ('a', 'b'), ('a', 'b', 'c'), 'd']
 """
 
+def task3(a_list):
+    return [item for item in a_list if len(item) > 0]
+
+
+print(task3([{}, '',  {'e': 13, 32: 'er'}, [34, "rer"], [], (1, 2), ()]))
