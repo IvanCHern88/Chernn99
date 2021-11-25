05599123db3c610514012ea413ff5f3f316db7cf
IvanCHern88 <92941716+IvanCHern88@users.noreply.github.com>
2021-11-25 17:14:08 +0200

    Update task_7.py

--- HT_3/task_7.py
+++ HT_3/task_7.py
@@ -1,2 +1,13 @@
 # 7. Ну і традиційно -> калькулятор :) повинна бути 1 ф-цiя яка б приймала 3 аргументи - один з яких операцiя, яку зробити!
 
+def func(x,y,str):
+   if str == '+':
+       return x + y
+   if str == '-':
+       return x - y
+   if str == '*':
+       return x * y
+   if str == '/':
+       return x / y
+
+print(func(4,3,'+'))
