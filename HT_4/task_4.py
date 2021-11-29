2e3ac6dcf5eaa3b81535589a3d85532accc51dcf
IvanCHern88 <92941716+IvanCHern88@users.noreply.github.com>
2021-11-29 17:34:14 +0200

    Update task_4.py

--- HT_4/task_4.py
+++ HT_4/task_4.py
@@ -1,1 +1,16 @@
 #  4. Написати функцію < prime_list >, яка прийматиме 2 аргументи - початок і кінець діапазона, і вертатиме список простих чисел всередині цього діапазона.
+
+def prime(n):
+    if n < 2:
+        return False
+    for i in [num for num in list(range(2, n+1)) if num*num <= n]:
+        if n % i == 0:
+            return False
+    return True
+
+
+def prime_list(first, last):
+    return [i for i in list(range(first, last+1)) if prime(i)]
+
+
+print(prime_list(41, 97))
