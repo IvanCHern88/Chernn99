19a9bd5e29cb6091ba81db60671f6de82b1f7bb1
IvanCHern88 <92941716+IvanCHern88@users.noreply.github.com>
2021-11-29 18:22:55 +0200

    Update task_7.py

--- HT_4/task_7.py
+++ HT_4/task_7.py
@@ -1,2 +1,10 @@
 # 7. Написати функцію, яка приймає на вхід список і підраховує кількість однакових елементів у ньому.
 
+def repeats(a_list):
+    repeat_dict = {key:0 for key in a_list}
+    for key in a_list:
+        repeat_dict[key] +=1
+    return repeat_dict
+
+
+print(repeats([1, 2, 3, 1, 4, 2, 2, 4, 6, 66, 454, 24, 5, 66, 4, 3]))
