a49e0d637d53c2d4ea417823f22375db2f57bc8c
IvanCHern88 <92941716+IvanCHern88@users.noreply.github.com>
2021-11-29 18:12:57 +0200

    Update task_3.py

--- HT_4/task_3.py
+++ HT_4/task_3.py
@@ -1,3 +1,20 @@
-3. Написати функцию < is_prime >, яка прийматиме 1 аргумент - число від 0 до 1000, и яка вертатиме True, якщо це число просте, и False - якщо ні.
+# 3. Написати функцию < is_prime >, яка прийматиме 1 аргумент - число від 0 до 1000, и яка вертатиме True, якщо це число просте, и False - якщо ні.
 
 
+from math import sqrt
+
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    limit = sqrt(n)
+    i = 2
+    while i <= limit:
+        if n % i == 0:
+            return False
+        i += 1
+    return True
+
+num = int(input())
+print(is_prime(num))
