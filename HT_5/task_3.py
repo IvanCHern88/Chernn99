13cb5f69711444db28fb01742880942ae316ca95
IvanCHern88 <92941716+IvanCHern88@users.noreply.github.com>
2021-12-02 18:32:08 +0200

    Update task_3.py

--- HT_5/task_3.py
+++ HT_5/task_3.py
@@ -12,3 +12,47 @@
    P.S. Не забудьте використати блок try/except ;)
    """
 
+class PasswordException(Exception):
+	def __init__(self, message):            
+		super().__init__(message)
+
+class User():
+
+	login = ""
+	password = ""
+	def __init__(self, login, password):
+		self.login = login
+		self.password = password
+
+def ValidateAuth(login, password):
+          if len(login) < 3 or len(login) > 50:
+                raise LoginException("Incorrect login size! Login should minimal contain 3 symbols and maximum is 50 symbols!")
+          if len(password) < 8:
+                raise PasswordException("Incorrect password size! Password should contain 8 symbols or more!")
+          if re.search('\d+', password) is None:
+                raise PasswordException("Incorrect password! Password should contain digits!")
+          if not "." in password:
+                raise PasswordException("Incorrect password! Password should contain one more DOT!")
+if __name__ == "__main__":
+	users = [
+			User("t", "testpassword"),
+			User("somelogin", "qwerty12345"),
+			User("helloworld123", "mypa.ss1337"),
+			User("12345", "ls12345"),
+			User("Mike1991", "m.ike1991")
+			]
+	for user in users:
+		print("Name: %s\nPassword: %s" % (user.login, user.password))
+		try:
+			ValidateAuth(user.login, user.password)
+			print("Status: OK!")
+		except LoginException:
+			print("Status: Login entering error!")
+		except PasswordException:
+			print("Status: Password entering error")
+		finally:
+			print("\n")
+
+
+
+
