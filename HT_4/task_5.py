6841845f5c056405dc3b5b2180522a1ee6fdf1c2
IvanCHern88 <92941716+IvanCHern88@users.noreply.github.com>
2021-12-02 15:09:52 +0200

    Update task_5.py

--- HT_4/task_5.py
+++ HT_4/task_5.py
@@ -1,21 +1,13 @@
 # 5. Написати функцію < fibonacci >, яка приймає один аргумент і виводить всі числа Фібоначчі, що не перевищують його.
 
 def fibonacci (count):
-    if count == 1 :
-        print('-')
-    fib1 = 1
-    fib2 = 1
-    sum = 2
-    i = 0
-    while fib2 != count:
-        sum_fib12 = fib1 + fib2
-        fib1 = fib2
-        fib2 = sum_fib12
-        sum += fib2
-        i += 1
-        print(fib2)
-
-
-
+    fib1 = fib2 = 1
+    n = count - 2
+    while n > 0 and fib1 < count:
+        fib1 , fib2 = fib2 , fib1 + fib2
+        if (fib1 < count):
+            print(f"{fib1}\n")
+        n -= 1
+        
 n = int(input("Введіть число Фібоначчі: "))
 fibonacci (n)
