41cb19c6676a899443219204ebe2ee025875a732
IvanCHern88 <92941716+IvanCHern88@users.noreply.github.com>
2021-12-06 18:11:41 +0200

    Update task_6.py

--- HT_5/task_6.py
+++ HT_5/task_6.py
@@ -3,3 +3,27 @@
    P.S. Повинен вертатись генератор.
    P.P.S. Для повного розуміння цієї функції - можна почитати документацію по ній: https://docs.python.org/3/library/stdtypes.html#range
 """
+
+def myrange(*args):
+    if len(args) == 1:
+        return myrange1(*args)
+    return myrange2_3(*args)
+
+def myrange1(end):
+    return myrange2_3(0,end)
+
+def myrange2_3(start,end,step=1):
+    start -= step
+    while True:
+        if start > end and step < 0:
+            if (start + step <= end):
+                break
+        else:
+            if (start + step >= end):
+                break
+        start += step
+        yield round(start, 2)
+
+print(*myrange(3,14))
+print(*myrange(3,-5,-2))
+print(*myrange(5))
